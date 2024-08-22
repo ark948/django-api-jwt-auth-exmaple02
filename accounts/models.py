d22ba65fddc31eaf058baaef2793dfa8ede99d0b
ark948 <armankarimi948@gmail.com>
2024-08-22 23:34:12 +0330

    commit 03 - bug fix - is_active must be true

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
 
