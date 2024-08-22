ad88dbd24deae1f2e050f00608575d3cb2d57021
ark948 <armankarimi948@gmail.com>
2024-08-22 22:56:14 +0330

    commit 01

---
+++ accounts/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=225, unique=True, verbose_name=_("Email Address"))
+    first_name = models.CharField(max_length=100, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=100, verbose_name=_("Last Name"))
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELDS = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.email
+    
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_login}"
+    
+    def tokens(self):
+        pass
