0d290530b061ad9079e8e6a57a80511d6b9bfa60
ark948 <armankarimi948@gmail.com>
2024-08-25 22:11:27 +0330

    frontend progress

---
+++ frontend/src/components/index.js
@@ -0,0 +1,15 @@
+import Signup from './Signup'
+import Login from './Login'
+import Profile from './Profile'
+import ForgotPassword from './ForgotPassword'
+import VerifyEmail from './VerifyEmail'
+
+// 04
+
+export {
+    Signup,
+    Login,
+    Profile,
+    ForgotPassword,
+    VerifyEmail
+}
